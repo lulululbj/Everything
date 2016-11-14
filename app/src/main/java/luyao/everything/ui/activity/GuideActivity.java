02816a58693df0d9cc196a13d924062fadad1635
Administrator <864588262@qq.com>
2016-11-14 18:58:09 +0800

    GuideActivity

---
+++ app/src/main/java/luyao/everything/ui/activity/GuideActivity.java
@@ -0,0 +1,27 @@
+package luyao.everything.ui.activity;
+
+import luyao.everything.R;
+import luyao.everything.base.BaseActivity;
+
+/**
+ * 引导用户选择需要的服务
+ * Created by Lu
+ * on 2016/11/14 18:56.
+ */
+
+public class GuideActivity extends BaseActivity {
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_guide;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
