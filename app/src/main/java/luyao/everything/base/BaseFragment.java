cfb854aab03e7c5376164417d1f1f0d93442c38f
Administrator <864588262@qq.com>
2016-11-30 20:00:26 +0800

    bug fix

---
+++ app/src/main/java/luyao/everything/base/BaseFragment.java
@@ -0,0 +1,14 @@
+package luyao.everything.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * 基类Fragment
+ * Created by Lu
+ * on 2016/11/30 19:59.
+ */
+
+public class BaseFragment extends Fragment {
+
+
+}
