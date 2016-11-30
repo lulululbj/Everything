ef1a1bd3febd365e658f36ca5c6c31f04d2134f0
Administrator <864588262@qq.com>
2016-11-30 20:05:07 +0800

    BaseFragment

--- app/src/main/java/luyao/everything/base/BaseFragment.java
+++ app/src/main/java/luyao/everything/base/BaseFragment.java
@@ -1,6 +1,16 @@
 package luyao.everything.base;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import luyao.everything.R;
+import luyao.everything.utils.ScreenUtil;
 
 /**
  * 基类Fragment
@@ -8,7 +18,33 @@ import android.support.v4.app.Fragment;
  * on 2016/11/30 19:59.
  */
 
-public class BaseFragment extends Fragment {
+public abstract class BaseFragment extends Fragment {
 
+    protected View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mView == null) {
+            mView = inflater.inflate(getLayoutResId(), container, false);
+            ScreenUtil.initScale(mView);
+            ButterKnife.bind(getActivity());
+            initView();
+            initData();
+
+        }
+        return mView;
+    }
+
+    protected abstract int getLayoutResId();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    protected void startActivity(Class z) {
+        startActivity(new Intent(getActivity(), z));
+        getActivity().overridePendingTransition(R.anim.slide_in_form_right, R.anim.slide_out_to_left);
+    }
 
 }
