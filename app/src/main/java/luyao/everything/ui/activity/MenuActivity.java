dda3d0c347c8c35787eddc1d879422e581b323dc
Administrator <864588262@qq.com>
2016-12-02 11:30:28 +0800

    侧滑完善

--- app/src/main/java/luyao/everything/ui/activity/MenuActivity.java
+++ app/src/main/java/luyao/everything/ui/activity/MenuActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import butterknife.BindView;
@@ -21,12 +22,18 @@ import luyao.everything.ui.fragment.MainFragment;
 
 public class MenuActivity extends BaseFragmentActivity {
 
-//
-//    @BindView(R.id.drawerLayout)
-//    DrawerLayout mDrawerLayout;
+
+    @BindView(R.id.drawerLayout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.left_content)
+    FrameLayout left_content;
+    @BindView(R.id.main_content)
+    FrameLayout main_content;
 
     private MainFragment mainFragment;
     private LeftFragment leftFragment;
+    private int mDrawerWidth;//抽屉全部拉出来时的宽度
+    private float scrollWidth;//抽屉被拉出部分的宽度
 
     @Override
     protected int getLayoutResId() {
@@ -37,6 +44,8 @@ public class MenuActivity extends BaseFragmentActivity {
     protected void initView() {
         if (mainFragment==null)mainFragment=new MainFragment();
         if (leftFragment==null)leftFragment=new LeftFragment();
+
+        initDrawerLayout();
     }
 
     @Override
@@ -49,5 +58,73 @@ public class MenuActivity extends BaseFragmentActivity {
         FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
         if (!fragment.isAdded())transaction.add(resId,fragment);
         transaction.commit();
+    }
+
+    private void initDrawerLayout(){
+        measureView(left_content);
+        mDrawerWidth = left_content.getMeasuredWidth();
+
+        mDrawerLayout.setScrimColor(0x00ffffff);
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                //因为arg1的范围是0.0-1.0，是一个相对整个抽屉宽度的比例
+                //所以要准换成
+                scrollWidth = slideOffset * mDrawerWidth;
+                //setScroll中的参数，正数表示向左移动，负数向右
+                main_content.setScrollX((int) (-1 * scrollWidth));
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
+
+    /**
+     * 此方法可以多次被不同的View对象调用。
+     * 在调用该方法后，
+     * 使用View对象的getMessuredHeight()获高度（单位px）
+     *
+     * @param child 需要测量高度和宽度的View对象，
+     */
+    private void measureView(View child) {
+        ViewGroup.LayoutParams params = child.getLayoutParams();
+        if (params == null) {
+            params = new ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+        }
+        int childWidthSpec = ViewGroup.getChildMeasureSpec(0, 0 + 0,
+                params.width);
+        int lpHeight = params.height;
+        int childHeightSpec;
+        if (lpHeight > 0) {
+            childHeightSpec = View.MeasureSpec.makeMeasureSpec(lpHeight,
+                    View.MeasureSpec.EXACTLY);
+        } else {
+            childHeightSpec = View.MeasureSpec.makeMeasureSpec(0,
+                    View.MeasureSpec.UNSPECIFIED);
+        }
+        child.measure(childWidthSpec, childHeightSpec);
+    }
+
+    private  void openMenu(){
+        if (mDrawerLayout.isDrawerOpen(left_content)){
+            mDrawerLayout.closeDrawer(left_content);
+        }else {
+            mDrawerLayout.openDrawer(left_content);
+        }
     }
 }
